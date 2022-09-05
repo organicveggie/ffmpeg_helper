413102621a492158bca82a105af0b6c8220a11ac
Sean Laurent <organicveggie@gmail.com>
2022-09-04 20:49:04 -0700

    First attempt at running mediainfo from GUI

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,33 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:logging/logging.dart';
+import 'package:provider/provider.dart';
+
+import 'mediainfo_exec.dart';
+import 'models/mediainfo.dart';
 
 void main() {
-  runApp(const MyApp());
+  Logger.root.onRecord.listen((record) {
+    print('${record.level.name}: ${record.time}: ${record.message}');
+  });
+  Logger.root.level = Level.ALL;
+
+  runApp(ChangeNotifierProvider(create: (context) => MediaModel(), child: const FFMpegHelperApp()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+enum MediaLoadingState {
+  none,
+  startLoading,
+  loading,
+  loaded,
+  error;
+}
+
+class FFMpegHelperApp extends StatelessWidget {
+  const FFMpegHelperApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
@@ -24,13 +46,13 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HelperHomePage(title: 'ffmpeg helper'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class HelperHomePage extends StatefulWidget {
+  const HelperHomePage({Key? key, required this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -44,72 +66,195 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HelperHomePage> createState() => _HelperHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HelperHomePageState extends State<HelperHomePage> {
+  @override
+  initState() {
+    super.initState();
+  }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void _pickMediaFile() async {
+    var model = context.read<MediaModel>();
+    FilePickerResult? result =
+        await FilePicker.platform.pickFiles(type: FileType.video, lockParentWindow: true);
+
+    if (result != null) {
+      if (result.files.isNotEmpty) {
+        var f = result.files.first;
+        model.filename = f.path ?? f.name;
+        model.loadingState = MediaLoadingState.startLoading;
+      }
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Consumer<MediaModel>(
+            builder: (context, model, child) {
+              switch (model.loadingState) {
+                case MediaLoadingState.startLoading:
+                  return MediaInfo(filename: model.filename!);
+                case MediaLoadingState.loaded:
+                  return Text('loaded ${model.filename}');
+                default:
+                  return const Text('no media file loaded');
+              }
+            },
+          ),
+        ],
       ),
+
+      // body: Center(
+      //   // Center is a layout widget. It takes a single child and positions it
+      //   // in the middle of the parent.
+      //   child: Column(
+      //     // Column is also a layout widget. It takes a list of children and
+      //     // arranges them vertically. By default, it sizes itself to fit its
+      //     // children horizontally, and tries to be as tall as its parent.
+      //     //
+      //     // Invoke "debug painting" (press "p" in the console, choose the
+      //     // "Toggle Debug Paint" action from the Flutter Inspector in Android
+      //     // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+      //     // to see the wireframe for each widget.
+      //     //
+      //     // Column has various properties to control how it sizes itself and
+      //     // how it positions its children. Here we use mainAxisAlignment to
+      //     // center the children vertically; the main axis here is the vertical
+      //     // axis because Columns are vertical (the cross axis would be
+      //     // horizontal).
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     children: <Widget>[
+      //       const Text(
+      //         'You have pushed the button this many times:',
+      //       ),
+      //       Text(
+      //         '$_counter',
+      //         style: Theme.of(context).textTheme.headline4,
+      //       ),
+      //     ],
+      //   ),
+      // ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: _pickMediaFile,
+        tooltip: 'Open Media',
+        child: const Icon(Icons.file_open),
+      ),
     );
+  }
+}
+
+class MediaModel extends ChangeNotifier {
+  MediaLoadingState _loadingState = MediaLoadingState.none;
+  String? _filename;
+
+  Future<MediaRoot>? _futureMediaRoot;
+  MediaRoot? _mediaRoot;
+
+  MediaLoadingState get loadingState => _loadingState;
+  set loadingState(MediaLoadingState state) {
+    _loadingState = state;
+    notifyListeners();
+  }
+
+  String? get filename => _filename;
+  set filename(String? filename) {
+    _filename = filename;
+    _loadingState = MediaLoadingState.loading;
+    notifyListeners();
+  }
+
+  Future<MediaRoot>? get futureMediaRoot => _futureMediaRoot;
+  set futureMediaRoot(Future<MediaRoot>? f) {
+    _futureMediaRoot = f;
+    _loadingState = (f == null) ? MediaLoadingState.none : MediaLoadingState.loading;
+    notifyListeners();
+  }
+
+  Media? get media => _mediaRoot?.media;
+
+  set mediaRoot(MediaRoot mediaRoot) {
+    _mediaRoot = mediaRoot;
+    notifyListeners();
+  }
+}
+
+class MediaPanel extends StatelessWidget {
+  const MediaPanel({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<MediaModel>(
+      builder: (context, model, child) {
+        switch (model.loadingState) {
+          case MediaLoadingState.loading:
+            return FutureBuilder<MediaRoot>(
+              future: model._futureMediaRoot,
+              builder: ((context, AsyncSnapshot<MediaRoot> snapshot) {
+                if (snapshot.hasData) {
+                  return Center(child: Text('Successfully loaded ${model.filename}'));
+                }
+                return Center(
+                    child: Column(children: <Widget>[
+                  Text('loading ${model.filename}'),
+                  const CircularProgressIndicator(),
+                ]));
+              }),
+            );
+          case MediaLoadingState.loaded:
+            return Text('loaded ${model.filename}');
+          case MediaLoadingState.error:
+            return Text('failed to load ${model.filename}');
+          default:
+            return const Text('no media file loaded');
+        }
+      },
+    );
+  }
+}
+
+class MediaInfo extends StatefulWidget {
+  MediaInfo({required this.filename, super.key});
+  final String filename;
+  Future<MediaRoot>? mediaRoot;
+
+  @override
+  State<StatefulWidget> createState() => _MediaInfoState();
+}
+
+class _MediaInfoState extends State<MediaInfo> {
+  _MediaInfoState();
+
+  final log = Logger('_MediaInfoState');
+
+  @override
+  void initState() {
+    super.initState();
+    widget.mediaRoot = runMediainfo(widget.filename);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: widget.mediaRoot!,
+        builder: (context, AsyncSnapshot<MediaRoot> snapshot) {
+          if (snapshot.hasData) {
+            return Center(child: Text('Successfully loaded ${widget.filename}'));
+          }
+          return Center(
+            child: Column(children: <Widget>[
+              Text('loading ${widget.filename}'),
+              const CircularProgressIndicator(),
+            ]),
+          );
+        });
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:ffmpeg_helper/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const FFMpegHelperApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
