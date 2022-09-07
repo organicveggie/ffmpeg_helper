492ee70eb775b81379818be0c129c0a88dc0e40d
Sean Laurent <organicveggie@gmail.com>
2022-09-06 22:01:26 -0700

    Use FileSelector dialog. Render basic metadata table.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-import 'dart:io';
-
-import 'package:file_picker/file_picker.dart';
-import 'package:flutter/foundation.dart';
+import 'package:file_selector/file_selector.dart';
 import 'package:flutter/material.dart';
 import 'package:logging/logging.dart';
 import 'package:provider/provider.dart';
@@ -24,6 +21,44 @@ enum MediaLoadingState {
   loading,
   loaded,
   error;
+}
+
+class MediaModel extends ChangeNotifier {
+  MediaLoadingState _loadingState = MediaLoadingState.none;
+  String? _filename;
+
+  MediaRoot? _mediaRoot;
+
+  MediaLoadingState get loadingState => _loadingState;
+  set loadingState(MediaLoadingState state) {
+    _loadingState = state;
+    notifyListeners();
+  }
+
+  String? get filename => _filename;
+  set filename(String? filename) {
+    _filename = filename;
+    _loadingState = MediaLoadingState.loading;
+    notifyListeners();
+  }
+
+  Media? get media => _mediaRoot?.media;
+
+  set mediaRoot(MediaRoot mediaRoot) {
+    _mediaRoot = mediaRoot;
+    notifyListeners();
+  }
+
+  Future<MediaRoot> loadFile(String filename) {
+    _filename = filename;
+    loadingState = MediaLoadingState.loading;
+
+    return runMediainfo(filename).then((mediaRoot) {
+      _mediaRoot = mediaRoot;
+      loadingState = MediaLoadingState.loaded;
+      return mediaRoot;
+    });
+  }
 }
 
 class FFMpegHelperApp extends StatelessWidget {
@@ -54,15 +89,6 @@ class FFMpegHelperApp extends StatelessWidget {
 class HelperHomePage extends StatefulWidget {
   const HelperHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -75,17 +101,16 @@ class _HelperHomePageState extends State<HelperHomePage> {
     super.initState();
   }
 
-  void _pickMediaFile() async {
+  void _selectMediaFile() async {
     var model = context.read<MediaModel>();
-    FilePickerResult? result =
-        await FilePicker.platform.pickFiles(type: FileType.video, lockParentWindow: true);
+    final XTypeGroup typeGroup = XTypeGroup(
+      label: 'movies',
+      extensions: <String>['mkv', 'mp4', 'm4v'],
+    );
 
-    if (result != null) {
-      if (result.files.isNotEmpty) {
-        var f = result.files.first;
-        model.filename = f.path ?? f.name;
-        model.loadingState = MediaLoadingState.startLoading;
-      }
+    final XFile? file = await openFile(acceptedTypeGroups: <XTypeGroup>[typeGroup]);
+    if (file != null) {
+      model.loadFile(file.path);
     }
   }
 
@@ -95,56 +120,34 @@ class _HelperHomePageState extends State<HelperHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Consumer<MediaModel>(
-            builder: (context, model, child) {
-              switch (model.loadingState) {
-                case MediaLoadingState.startLoading:
-                  return MediaInfo(filename: model.filename!);
-                case MediaLoadingState.loaded:
-                  return Text('loaded ${model.filename}');
-                default:
-                  return const Text('no media file loaded');
-              }
-            },
-          ),
-        ],
+      body: Consumer<MediaModel>(
+        builder: (context, model, child) {
+          switch (model.loadingState) {
+            case MediaLoadingState.loading:
+              return Column(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+                Expanded(
+                    child: Container(
+                  padding: const EdgeInsets.all(5.0),
+                  child: Column(children: <Widget>[
+                    Text('loading ${model.filename}'),
+                    const CircularProgressIndicator(),
+                  ]),
+                ))
+              ]);
+            case MediaLoadingState.loaded:
+              return const MediaInfoPanel();
+            default:
+              return Column(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+                Expanded(
+                    child: Container(
+                        padding: const EdgeInsets.all(5.0),
+                        child: const Text('no media file loaded')))
+              ]);
+          }
+        },
       ),
-
-      // body: Center(
-      //   // Center is a layout widget. It takes a single child and positions it
-      //   // in the middle of the parent.
-      //   child: Column(
-      //     // Column is also a layout widget. It takes a list of children and
-      //     // arranges them vertically. By default, it sizes itself to fit its
-      //     // children horizontally, and tries to be as tall as its parent.
-      //     //
-      //     // Invoke "debug painting" (press "p" in the console, choose the
-      //     // "Toggle Debug Paint" action from the Flutter Inspector in Android
-      //     // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-      //     // to see the wireframe for each widget.
-      //     //
-      //     // Column has various properties to control how it sizes itself and
-      //     // how it positions its children. Here we use mainAxisAlignment to
-      //     // center the children vertically; the main axis here is the vertical
-      //     // axis because Columns are vertical (the cross axis would be
-      //     // horizontal).
-      //     mainAxisAlignment: MainAxisAlignment.center,
-      //     children: <Widget>[
-      //       const Text(
-      //         'You have pushed the button this many times:',
-      //       ),
-      //       Text(
-      //         '$_counter',
-      //         style: Theme.of(context).textTheme.headline4,
-      //       ),
-      //     ],
-      //   ),
-      // ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _pickMediaFile,
+        onPressed: _selectMediaFile,
         tooltip: 'Open Media',
         child: const Icon(Icons.file_open),
       ),
@@ -152,109 +155,48 @@ class _HelperHomePageState extends State<HelperHomePage> {
   }
 }
 
-class MediaModel extends ChangeNotifier {
-  MediaLoadingState _loadingState = MediaLoadingState.none;
-  String? _filename;
-
-  Future<MediaRoot>? _futureMediaRoot;
-  MediaRoot? _mediaRoot;
-
-  MediaLoadingState get loadingState => _loadingState;
-  set loadingState(MediaLoadingState state) {
-    _loadingState = state;
-    notifyListeners();
-  }
-
-  String? get filename => _filename;
-  set filename(String? filename) {
-    _filename = filename;
-    _loadingState = MediaLoadingState.loading;
-    notifyListeners();
-  }
-
-  Future<MediaRoot>? get futureMediaRoot => _futureMediaRoot;
-  set futureMediaRoot(Future<MediaRoot>? f) {
-    _futureMediaRoot = f;
-    _loadingState = (f == null) ? MediaLoadingState.none : MediaLoadingState.loading;
-    notifyListeners();
-  }
-
-  Media? get media => _mediaRoot?.media;
-
-  set mediaRoot(MediaRoot mediaRoot) {
-    _mediaRoot = mediaRoot;
-    notifyListeners();
-  }
-}
-
-class MediaPanel extends StatelessWidget {
-  const MediaPanel({Key? key}) : super(key: key);
+class MediaInfoPanel extends StatelessWidget {
+  const MediaInfoPanel({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<MediaModel>(
-      builder: (context, model, child) {
-        switch (model.loadingState) {
-          case MediaLoadingState.loading:
-            return FutureBuilder<MediaRoot>(
-              future: model._futureMediaRoot,
-              builder: ((context, AsyncSnapshot<MediaRoot> snapshot) {
-                if (snapshot.hasData) {
-                  return Center(child: Text('Successfully loaded ${model.filename}'));
-                }
-                return Center(
-                    child: Column(children: <Widget>[
-                  Text('loading ${model.filename}'),
-                  const CircularProgressIndicator(),
-                ]));
-              }),
-            );
-          case MediaLoadingState.loaded:
-            return Text('loaded ${model.filename}');
-          case MediaLoadingState.error:
-            return Text('failed to load ${model.filename}');
-          default:
-            return const Text('no media file loaded');
-        }
-      },
+    var model = context.read<MediaModel>();
+    var tracklist = model.media!.trackList;
+    var audioTracklist = tracklist.audioTracks;
+    var videoTracklist = tracklist.videoTracks;
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('Filename: ${model.filename}'),
+        Expanded(
+            child: DataTable(columns: const <DataColumn>[
+          DataColumn(label: Expanded(child: Text('Type'))),
+          DataColumn(label: Expanded(child: Text('Format'))),
+          DataColumn(label: Expanded(child: Text('Language'))),
+          DataColumn(label: Expanded(child: Text('Notes'))),
+        ], rows: <DataRow>[
+          ...List<DataRow>.generate(
+              videoTracklist.length,
+              (index) => DataRow(cells: <DataCell>[
+                    const DataCell(Text('Video')),
+                    DataCell(Text(videoTracklist[index].format)),
+                    const DataCell(Text('n/a')),
+                    DataCell(
+                        Text('${videoTracklist[index].sizeName} ${videoTracklist[index].hdrName}'))
+                  ])),
+          ...List<DataRow>.generate(
+              audioTracklist.length,
+              (index) => DataRow(cells: <DataCell>[
+                    const DataCell(Text('Audio')),
+                    DataCell(Text(audioTracklist[index].format)),
+                    DataCell(Text(audioTracklist[index].language ?? 'unknown')),
+                    DataCell(Text(
+                        '${audioTracklist[index].channels} channels, ${audioTracklist[index].bitRateMaxAsKbps} kbps'))
+                  ]))
+        ]))
+      ],
     );
-  }
-}
-
-class MediaInfo extends StatefulWidget {
-  MediaInfo({required this.filename, super.key});
-  final String filename;
-  Future<MediaRoot>? mediaRoot;
-
-  @override
-  State<StatefulWidget> createState() => _MediaInfoState();
-}
-
-class _MediaInfoState extends State<MediaInfo> {
-  _MediaInfoState();
-
-  final log = Logger('_MediaInfoState');
-
-  @override
-  void initState() {
-    super.initState();
-    widget.mediaRoot = runMediainfo(widget.filename);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: widget.mediaRoot!,
-        builder: (context, AsyncSnapshot<MediaRoot> snapshot) {
-          if (snapshot.hasData) {
-            return Center(child: Text('Successfully loaded ${widget.filename}'));
-          }
-          return Center(
-            child: Column(children: <Widget>[
-              Text('loading ${widget.filename}'),
-              const CircularProgressIndicator(),
-            ]),
-          );
-        });
   }
 }
