8b78244fd4e80ac3cce6ac2df1a38b4b7961d2cf
Sean Laurent <organicveggie@gmail.com>
2022-10-01 23:43:09 -0700

    Remove builtvalue_jsonserializable test code

--- builtvalue_jsonserializable/lib/builder.dart
+++
@@ -1,9 +0,0 @@
-library builtvalue_jsonserializable.builder;
-
-import 'package:build/build.dart';
-import 'package:source_gen/source_gen.dart';
-
-import 'src/builtvalue_jsonserializable_generator.dart';
-
-Builder builtValueJsonSerializableBuilder(BuilderOptions options) =>
-    SharedPartBuilder([BuiltValueJsonSerializableGenerator()], 'bvjs');
