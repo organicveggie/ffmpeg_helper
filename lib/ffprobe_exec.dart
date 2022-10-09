bf43a4929fd2ce0447965165b5230ae67d85ad2f
Sean Laurent <organicveggie@gmail.com>
2022-10-09 09:58:02 -0700

    Remove unused ffprobe code

--- lib/ffprobe_exec.dart
+++
@@ -1,5 +0,0 @@
-const _defaultFfprobeBin = '/usr/bin/ffprobe';
-
-Future<void> runFfprobe(String pathname, {String? ffprobeBin = _defaultFfprobeBin}) async {
-  // fprobe -v quiet -print_format json -show_format -show_streams "lolwut.mp4"
-}
